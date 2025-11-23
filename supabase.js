afbacae321843b58c1c63fda9786fcccd941d51c
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2025-11-24 02:16:56 +0530

    supabase

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   "https://zyqdgevlhmsryuxknbly.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5cWRnZXZsaG1zcnl1eGtuYmx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4MDQzNTUsImV4cCI6MjA3OTM4MDM1NX0.1QlTUxXzHjUWflVY-Bhwb-WkD_JipcuQDZixU87CY-4",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5cWRnZXZsaG1zcnl1eGtuYmx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5MjU2NjAsImV4cCI6MjA3OTI4NTY2MH0.TUOKPBkZy0lZ0AXIh1ptgqtDqspufLGbaOhIhZG6OYQ",
   {
     auth: {
       persistSession: true,
